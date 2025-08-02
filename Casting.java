5b8d3e8713fceef76524d08e46cb4fa63ac93a4d
JaguMadhura <2420030535@klh.edu.in>
2025-08-02 10:39:59 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,18 @@
+package Madhura;
+
+public class Casting {
+	public static void main(String[] args) 
+	{
+	 int i = 10;
+ 
+	long l = i;
+	 
+	 double d = i;
+	 System.out.println("Integer: " + i); 
+	System.out.println("Long: " + l); 
+	System.out.println("Double: " + d); 
+	}
+	}
+
+
+
