6e8959e970e198aca0dcb641b5dcb4f4d923f70b
JaguMadhura <2420030535@klh.edu.in>
2025-08-02 10:40:26 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,25 @@
+    
+package Madhura;
+
+public class Datatypes {
+    public static void main(String args[])
+    {
+        char a = 'V';
+        int i = 90;
+        byte b = 4;
+        short s = 56;
+        double d = 7.98094877;
+        float f = 4.86738f;
+        long l = 1212;
+        System.out.println("char: " + a);
+        System.out.println("integer: " + i);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("long: " + l);
+    }
+}
+
+
+
