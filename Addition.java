fd49c3dcafdf92e5c0f756d46de259ceb7343eeb
JaguMadhura <2420030535@klh.edu.in>
2025-08-02 10:39:13 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,23 @@
+package Madhura;
+import java.util.Scanner;
+public class Addition {
+	
+
+
+		public static void main(String[] args) {
+			int x,y,sum;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Type a numer:");
+			x = sc.nextInt();
+			System.out.println("Type another number:");
+			y = sc.nextInt();
+			sum = x + y;
+			System.out.println("Sum is: "+ sum);
+			
+		
+
+	}
+}
+
+
+
