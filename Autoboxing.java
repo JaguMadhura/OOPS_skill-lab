5025b538b0c6a00059e68e454896cef2d698d350
JaguMadhura <2420030535@klh.edu.in>
2025-08-02 10:39:45 +0530

    Add files via upload

---
+++ Autoboxing.java
@@ -0,0 +1,14 @@
+package Madhura;
+
+public class Autoboxing {
+	 
+	public static void main(String args[])
+	{  
+	    int a=50;  
+	   Integer i=Integer.valueOf(a);  
+	   Integer j=a;   
+	   System.out.println(a+" "+i+" "+j);  
+	  }
+	}  
+
+
