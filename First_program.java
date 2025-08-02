162304ba78a10c3ac5d12f14904c7879c6492f7e
JaguMadhura <2420030535@klh.edu.in>
2025-08-02 10:41:19 +0530

    Add files via upload

---
+++ First_program.java
@@ -0,0 +1,13 @@
+package Madhura;
+
+public class First_program {
+
+	class Example 
+	{
+	   public static void main(String args[]) 
+	  {
+	   System.out.println("This is a simple Java program.");
+	  }   
+	}
+
+}
